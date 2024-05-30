cabd20a5b682e5e560279d405b564e11f174ea4a
msun0041 <msun0041@student.monash.edu>
2024-05-30 16:03:55 +0800

    Modified table to have header, body and pagination

--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -10,8 +10,10 @@ import {
   TableBody,
   TableCell,
   TableRow,
+  TableHead,
   Paper,
   TableContainer,
+  TablePagination,
   CircularProgress,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -31,12 +33,15 @@ const Dashboard: React.FC = () => {
   const [error, setError] = useState<string | null>(null);
   // State for progress indicator while data is being fetched
   const [loading, setLoading] = useState<boolean>(true);
+  // State for pagination
+  const [page, setPage] = useState<number>(0);
+  const [rowsPerPage, setRowsPerPage] = useState<number>(5);
 
   useEffect(() => {
     axios
       .get('https://jsonplaceholder.typicode.com/posts', {
         params: {
-          _limit: 20, // GET 20 posts from the API
+          _limit: 55, // GET 55 posts from the API
         },
       })
       .then((response) => {
@@ -48,6 +53,16 @@ const Dashboard: React.FC = () => {
         setLoading(false); // Set loading to false if error
       });
   }, []);
+
+  // Function to handle page change
+  const handleChangePage = (_: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+  // Function to handle RowsPerPage change
+  const handleChangeRowsPerPage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(e.target.value, 10)); // Update rows per page state
+    setPage(0); // Reset page to 0 when rows per page changes
+  };
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -75,25 +90,46 @@ const Dashboard: React.FC = () => {
         ) : error ? (
           <Typography color="error">{error}</Typography>
         ) : (
-          <TableContainer component={Paper}>
-            <Table>
-              <TableBody>
-                {posts.map((post) => (
-                  <React.Fragment key={post.id}>
-                    <TableRow>
-                      <TableCell>ID: {post.userId}</TableCell>
-                      <TableCell>
-                        <h4>Title: {post.title}</h4>
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell colSpan={2}>{post.body}</TableCell>
-                    </TableRow>
-                  </React.Fragment>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+          <React.Fragment>
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead sx={{ backgroundColor: '#f0f0f0' }}>
+                  <TableRow>
+                    <TableCell>No.</TableCell>
+                    <TableCell>User</TableCell>
+                    <TableCell>Title</TableCell>
+                    <TableCell>Body</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {posts
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((post, index) => (
+                      <TableRow key={post.id}>
+                        <TableCell>
+                          {
+                            <b>{page * rowsPerPage + index + 1}</b> // Calculate overall index for the post numbering
+                          }
+                        </TableCell>
+                        <TableCell>{post.userId}</TableCell>
+                        <TableCell>{post.title}</TableCell>
+                        <TableCell>{post.body}</TableCell>
+                      </TableRow>
+                    ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            {/* ------- Table Pagination --------*/}
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              component="div"
+              count={posts.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </React.Fragment>
         )}
       </Box>
     </Box>
